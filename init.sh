c82d7554b6aa7d5837f72ee3f4d245914809347f
Ivan <Kozhukhov>
2020-05-05 21:13:13 +0300

    add log for mysql 8

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-FILE="/var/www/environment/docker/build/mysql-8/log/log.err"
+FILE="/var/www/environment/docker/build/mysql-8/log/error.log"
 if ! [ -f $FILE ]; then
     touch $FILE
+    chmod 0666 $FILE
     echo the file $FILE was created
 fi
